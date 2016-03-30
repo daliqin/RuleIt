09cdfd5ea19f58b8f88b367946a8cb425f64f87a
Qin, Charles <Charles.X.Qin.-ND@disney.com>
2016-03-30 11:16:10 -0700

    Initial Commit

---
+++ RuleIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuleIt
+//
+//  Created by Qin, Charles on 3/30/16.
+//  Copyright © 2016 RuleIt Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
