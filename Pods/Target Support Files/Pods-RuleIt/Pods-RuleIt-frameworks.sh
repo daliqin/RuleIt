40d889fca394dcfc96c5ba6a2aa261220a02d948
Qin, Dali-CW <Charles.Chin@bestbuy.com>
2016-07-25 10:06:12 -0700

    running fine now on BBY computer

--- Pods/Target Support Files/Pods-RuleIt/Pods-RuleIt-frameworks.sh
+++ Pods/Target Support Files/Pods-RuleIt/Pods-RuleIt-frameworks.sh
@@ -85,10 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Instructions/Instructions.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Instructions/Instructions.framework"
 fi
 if [[ "$CONFIGURATION" == "Distribution" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Instructions/Instructions.framework"
 fi
